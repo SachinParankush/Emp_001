d68cbe0edb7e942bdff28e6f30527618b6cac916
SachinParankush <sachisachins24@gmail.com>
2019-06-04 16:58:43 +0530

    dashboard
    dashboard

--- src/app/empireModule/dashboard/dashboard.component.ts
+++ src/app/empireModule/dashboard/dashboard.component.ts
@@ -6,10 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
+  
+  cardDetails = [
+    {
+    "HotelName":"Empire",
+    "Address":"Next to BSNL, 80 Feet Rd, HAL 2nd Stage, Indiranagar, Bengaluru, Karnataka 560038",
+    "id":"11",
+    "location":"Indiranagar",
+    "image":"../../../assets/img/empire-hotel.png"
+  },
+  {
+    "HotelName":"Empire",
+    "Address":"Near, Plot No. 103 1st Cross Rd, Jyoti Nivas College Rd, Industrial Area, 5th Block, Koramangala, Bengaluru, Karnataka 560095",
+    "id":"12",
+    "location":"Koramangala",
+    "image":"../../../assets/img/vegetarea.png"
+  },
+  {
+    "HotelName":"Empire",
+    "Address":"36, Church St, Shanthala Nagar, Ashok Nagar, Bengaluru, Karnataka 560001",
+    "id":"13",
+    "location":"Ashok Nagar",
+    "image":"../../../assets/img/karama.png"
+  },
+  {
+    "HotelName":"Empire",
+    "Address":"36, Church St, Shanthala Nagar, Ashok Nagar, Bengaluru, Karnataka 560001",
+    "id":"13",
+    "location":"Ashok Nagar",
+    "image":"../../../assets/img/empire-hotel.png"
+  },
+  ]
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getMenu(data){
+    alert(data);
   }
 
 }
