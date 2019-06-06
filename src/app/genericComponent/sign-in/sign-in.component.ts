10bf354d31aeec184ee5c178cb5da0261507aecb
pushpalatha m.j <pushpalathacs407@gmail.com>
2019-06-06 11:12:10 -0600

    login and rigister form page development
    login and rigister form page development

--- src/app/genericComponent/sign-in/sign-in.component.ts
+++ src/app/genericComponent/sign-in/sign-in.component.ts
@@ -10,17 +10,34 @@ export class SignInComponent implements OnInit {
 
   title = 'Angular Form';
   angForm: FormGroup;
+  submitted = false;
+
 
   constructor(private fb: FormBuilder) {
 
-    this.angForm = this.fb.group({
-      name: ['', Validators.required ],
-      address: ['', Validators.required ]
-   });
+    
     
   }
 
   ngOnInit() {
+    this.angForm = this.fb.group({
+      
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      
+   });
 
+  }
+  get f() { return this.angForm.controls; }
+
+  onSubmit() {
+      this.submitted = true;
+
+      // stop here if form is invalid
+      if (this.angForm.invalid) {
+          return;
+      }
+
+      alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.angForm.value))
   }
 }
