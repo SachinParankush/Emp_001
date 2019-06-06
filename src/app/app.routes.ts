c5006bf7f314a5d98a9620c9ff302558afa02e50
SachinParankush <sachisachins24@gmail.com>
2019-06-06 11:13:01 +0530

    Routing Module Modified
    Routing Module Modified

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 export const ROUTES: Routes = [
   
-  { path: '', redirectTo: 'routesNoMenu', pathMatch: 'full' },
+  { path: '', redirectTo: 'empire', pathMatch: 'full' },
   { path: 'empire', loadChildren: './empireModule/empire.module#empireModule' },
   { path: 'routesNoMenu', loadChildren: './empireNoMenu/empireNoMenu.module#empireNoModule' },
   { path: 'menuDetails', loadChildren: './menu/menu.module#menuModule' },
   
   // Not found
-  { path: '**', redirectTo: 'routesNoMenu' }
+  { path: '**', redirectTo: 'empire' }
 ];
