2ee7ad52314e536bb359b2f4f360c6e42c6ba673
pushpalatha m.j <pushpalathacs407@gmail.com>
2019-06-13 12:50:48 -0600

    Correction
    Correction

--- src/app/providers/config.ts
+++ src/app/providers/config.ts
@@ -3,10 +3,9 @@ export const SERVERTYPE = "DEVELOPMENT"; // for server Type should be "PRODUCTIO
 export const SIGNUP = "/api/specific/signUp";
 export const SIGNIN = "http://192.168.1.181:5000/login";
 
-
 export const CREATEDATA = "/api/mysqlDb/createData";
 export const UPADTEDATA = "/api/mysqlDb/updateData";
-export const RETRIEVEBYKEY ="/api/mysqlDb/retrieveByKey";
+export const RETRIEVEBYKEY = "/api/mysqlDb/retrieveByKey";
 export const RETRIEVEALL = "http://192.168.1.181:8080/empireh/angular_callcenter_api/get_orders";
 export const RETRIVEMENUDATA = "http://192.168.1.181:8080/empireh/angular_callcenter_api/menu_list";
 export const RETRIVEOUTOFSTOCKDATA = "http://192.168.1.181:8080/empireh/angular_callcenter_api/menu_list";
@@ -15,4 +14,4 @@ export const ORDERSTATUSCHANGE = "http://192.168.1.181:8080/empireh/angular_call
 export const GETORDERDATA = "http://192.168.1.181:8080/empireh/angular_callcenter_api/get_orders_by_status";
 export const GETORDERCOUNT = "http://192.168.1.181:8080/empireh/angular_callcenter_api/get_status_count";
 export const DELETEDATA = "/api/mysqlDb/deleteData";
-export const REGISTER = "http://192.168.1.181:5000/register"
+export const REGISTER = "http://192.168.1.181:5000/register";
