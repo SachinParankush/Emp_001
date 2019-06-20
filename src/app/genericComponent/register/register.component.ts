e8aaac43edca502b03531a84e6ebda5212f4725d
pushpalatha m.j <pushpalathacs407@gmail.com>
2019-06-20 17:02:11 -0600

    Validation For register and Login
    Validation For register and Login

--- src/app/genericComponent/register/register.component.ts
+++ src/app/genericComponent/register/register.component.ts
@@ -35,16 +35,16 @@ export class RegisterComponent implements OnInit {
   constructor(private formBuilder: FormBuilder, public EmpireApiService: empireApiService) {
     this.getAddressList();
     this.registerForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      mobileNumber: ['', [Validators.required, Validators.minLength(10)]],
-      emailId: ['', [Validators.required,]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
+      name: ['', [Validators.required, Validators.maxLength(30)]],
+      mobileNumber: ['', Validators.compose([Validators.required, Validators.maxLength(12), Validators.minLength(10),Validators.pattern('[0-9]+')])],
+      emailId: ['', [Validators.required,Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(15)]],
       city: ['', [Validators.required]],
       area: ['', [Validators.required]],
-      street: ['', Validators.required],
-      doorNumber: ['', Validators.required],
-      landMark: ['', Validators.required],
-      fullAddress: ['', Validators.required],
+      street: ['', [Validators.required, Validators.maxLength(20)]],
+      doorNumber: ['', [Validators.required, Validators.maxLength(6)]],
+      landMark: ['', [Validators.required,Validators.maxLength(50)]],
+      fullAddress: ['', [Validators.required,Validators.maxLength(50)]],
     });
   }
 
--- src/app/genericComponent/sign-in/sign-in.component.ts
+++ src/app/genericComponent/sign-in/sign-in.component.ts
@@ -13,52 +13,44 @@ import { CookieService } from 'ngx-cookie-service';
 export class SignInComponent implements OnInit {
 
   title = 'Angular Form';
-  angForm: FormGroup;
+  loginForm: FormGroup;
   submitted = false;
   cookieValue = 'UNKNOWN';
 
-
-  constructor( private cookie: CookieService,private fb: FormBuilder, private router: Router, private EmpireApiService: empireApiService, private EmpireAppState: AppState) {
+  constructor(private cookie: CookieService,private fb: FormBuilder,private router: Router, private EmpireApiService:empireApiService,private EmpireAppState: AppState) {
+    this.loginForm = this.fb.group({      
+      mobile_no: ['', [Validators.required,Validators.maxLength(12),Validators.minLength(10),Validators.pattern('[0-9]+')]],
+      password: ['', [Validators.required,Validators.maxLength(15),Validators.minLength(6)]],
+      
+   });
 
   }
 
   ngOnInit() {
-    this.angForm = this.fb.group({
-
-      mobile_no: ['', [Validators.required]],
-      password: ['', [Validators.required]],
-
-    });
-
 
   }
-  get f() { return this.angForm.controls; }
 
   onSubmit() {
-    this.submitted = true;
+      this.submitted = true;
 
-    if (this.angForm.valid) {
-      var params = {
-        "mobile_no": this.angForm.value.mobile_no,
-        "password": this.angForm.value.password,
+      if (this.loginForm.valid) {
+        var params = {
+          "mobile_no": this.loginForm.value.mobile_no,
+          "password": this.loginForm.value.password,
+        }
+        this.EmpireApiService.signIn(params).subscribe(
+          (res: any) => { 
+            if(res.code==200){
+            this.EmpireAppState.user_id = res.user_id.toString();
+            this.EmpireAppState.mobile_no = res.mobile_no;
+            this.router.navigate(['/empire/Address']);
+            this.cookie.set( this.EmpireAppState.user_id, this.EmpireAppState.mobile_no );
+            console.log(this.cookie.get(this.EmpireAppState.mobile_no ));
+          }
+          else{
+            alert("Ivalid User Name or Password")
+          }
+          })
       }
-      this.EmpireApiService.signIn(params).subscribe(
-        (res: any) => {
-          console.log(JSON.stringify(res));
-          if(res.code==200){
-          this.EmpireAppState.user_id = res.user_id.toString();
-          this.EmpireAppState.mobile_no = res.mobile_no;
-          this.router.navigate(['/empire/Address']);
-          this.cookie.set( this.EmpireAppState.user_id, this.EmpireAppState.mobile_no );
-          console.log(this.cookie.get(this.EmpireAppState.mobile_no ));
-        }
-        else{
-          alert("Ivalid User Name or Password")
-        }
-        })
-    }
-    
   }
-  
-  
 }
